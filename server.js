d62f3761b0c4f329cb2a8da2ea94a010347459c9
Mason Newell <mason.newell13@gmail.com>
2022-01-29 15:58:57 -0800

    setup server, added routes index

---
+++ server.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(require("./controllers/index"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = db;
