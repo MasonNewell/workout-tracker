651c1ad3d0c08eb69c8e2f3ffe5c315e13291cad
Mason Newell <mason.newell13@gmail.com>
2022-01-29 16:33:07 -0800

    added workout routes

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,5 +1,24 @@
 const router = require("express").Router();
 // models
-// routes
+// ======= /api/workouts
+
+// Get all workouts
+router.get("/", (req, res) => {
+  res.json("hello world");
+});
+
+// ????    but must be before /:id
+router.get("/range", (req, res) => {
+  res.json("hello api/workouts/range");
+});
+// Get workout by ID
+router.get("/:id", (req, res) => {
+  res.json("hello ID world");
+});
+
+// Post new workout
+router.post("/", (req, res) => {
+  // return ...
+});
 
 module.exports = router;
